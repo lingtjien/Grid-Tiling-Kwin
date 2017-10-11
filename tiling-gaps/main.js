36288586c669c67e5c1a76d0bfad2e21289c67af
lingtjien <lingtjien@gmail.com>
2017-10-11 23:11:37 +0200

    finish and tested Tile and Desktop classes

--- tiling-gaps/main.js
+++ tiling-gaps/main.js
@@ -61,54 +61,62 @@ var ignored_captions =
 // Data Definitions
 // ----------------
 
-function Tile (window_id, type)
+function Tile (window_id, size)
 {
   this.window_id = window_id; //must be unique for every window
-  this.type = type; //either 1, 0.5 or 0.25
+  this.size = size; //max 1, which is completely fill one desktop
 };
 
-// every tiled desktop has at max 4 tiles per layer
-function TiledDesktop ()
+function Desktop ()
 {
-  this.filled = false;
   this.tiles = [];
   this.ntiles = function () {return this.tiles.length;};
+  this.size = function ()
+  {
+    sum = 0;
+    for (i = 0; i < this.ntiles(); i++) {sum += this.tiles[i].size;};
+    return sum;
+  };
   
   this.addTile = function (tile)
   {
-    if (this.filled) {return -1;};
+    if (this.size() === 1) {return -1;};
     this.tiles.push(tile);
+    return 0;
   };
   
   this.removeTile = function (window_id)
   {
     if (this.ntiles === 0) {return -1;};
-    if (this.tiles.includes(window_id))
+    
+    for (i = 0; i < this.size(); i++)
     {
-      this.tiles.splice(this.tiles.window_id.indexOf(window_id), 1);
-      this.filled = false;
-    } else {return -1;};
-  };
-  
-  this.largestAvailableSize = function ()
-  {
-    if (this.filled) {return -1;}
-    
-    
+      if (this.tiles[i].window_id === window_id)
+      {
+        print('hihi');
+        this.tiles.splice(i, 1);
+        return 0;
+      };
+    };
+    return -1;
   };
 };
 
 // all virtual desktops are contained in one layer
 function Layer ()
 {
-  this.filled = false;
   this.desktops = [];
   this.ndesktops =  function () {return this.desktops.length;};
+  this.size = function ()
+  {
+    return workspace.desktops-this.ndesktops;
+  };
   
   this.addDesktop = function (desktop)
   {
-    if (this.filled) {return -1;};
+    if (this.availableSize == 0) {return -1;};
     this.desktop.push(desktop);
+    return 0;
   };
   
   this.removeTile = function (window_id)
@@ -117,10 +125,44 @@ function Layer ()
     for (i = 0; i < this.ndesktops; i++)
     {
       check = this.desktops[i].removeTile(window_id);
-      if (check === -1) {continue;};
-      break;
+      if (check === 0) {break;};
     };
     return check;
+  };
+  
+  //returns the first desktop that has enough space
+  this.availableDesktopId = function (size)
+  {
+    desktop = -1;
+    for (i = 0; i < this.ndesktops; i++)
+    {
+      if (this.desktops[i].availableSize() > size)
+      {
+        desktop = i;
+        break;
+      }
+      return desktop;
+    };
+    
+  };
+};
+
+function Layout ()
+{
+  this.layers = [];
+  this.nlayers = function () {return this.layers.length;};
+  
+  this.addLayer = function (layer) 
+  {
+    this.layers.push(layer);
+    return 0;
+  };
+  
+  this.addDesktop = function (desktop)
+  {
+    
+    
+    return 0;
   };
 };
 
@@ -146,6 +188,14 @@ workspace.clientAdded.connect
 //     print(client.geometry.y)
 //     print(client.geometry.width)
 //     print(client.geometry.height)
+    
+    var tile = new Tile(client.windowId, 0.5);
+    
+    var desktop = new Desktop();
+    desktop.addTile(tile);
+    desktop.removeTile(client.windowId);
+    
+    
     
     
   }
