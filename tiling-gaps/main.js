c4190b0817c33bfe8d59eda9af9d4e9fa651f217
lingtjien <lingtjien@gmail.com>
2017-10-14 19:34:13 +0200

    finish classes

--- tiling-gaps/main.js
+++ tiling-gaps/main.js
@@ -57,14 +57,14 @@ var ignored_captions =
   "QEMU",
 ];
 
-// ----------------
-// Data Definitions
-// ----------------
+// -----------------
+// Class Definitions
+// -----------------
 
 function Tile (window_id, size)
 {
-  this.window_id = window_id; //must be unique for every window
-  this.size = size; //max 1, which is completely fill one desktop
+  this.window_id = window_id; // must be unique for every window
+  this.size = size; // max 1, which is completely fill one desktop
 };
 
 function Desktop ()
@@ -73,9 +73,14 @@ function Desktop ()
   this.ntiles = function () {return this.tiles.length;};
   this.size = function ()
   {
-    sum = 0;
-    for (i = 0; i < this.ntiles(); i++) {sum += this.tiles[i].size;};
+    var sum = 0;
+    for (var i = 0; i < this.ntiles(); i++) {sum += this.tiles[i].size;};
     return sum;
+  };
+  this.available = function (size)
+  {
+    if (1 - this.size() >= size) {return 0;};
+    return -1;
   };
   
   this.addTile = function (tile)
@@ -87,16 +92,16 @@ function Desktop ()
   
   this.removeTile = function (tile_index)
   {
-    if (tile_index > this.ntiles()) {return -1;};
+    if (tile_index >= this.ntiles()) {return -1;};
     this.tiles.splice(tile_index, 1);
     return 0;
   };
   
-  this.removeWindowId = function (window_id)
+  this.removeTileWindowId = function (window_id)
   {
     if (this.ntiles() === 0) {return -1;};
     
-    for (i = 0; i < this.ntiles(); i++)
+    for (var i = 0; i < this.ntiles(); i++)
     {
       if (this.tiles[i].window_id === window_id)
       {
@@ -113,46 +118,62 @@ function Layer ()
 {
   this.desktops = [];
   this.ndesktops =  function () {return this.desktops.length;};
+  this.size = function () {return this.ndesktops() / workspace.desktops;};
+  
+  this.availableId = function (size)
+  {
+    for (var i = 0; i < this.ndesktops(); i++)
+    {
+      if (this.desktops[i].available(size) === 0) {return i;};
+    };
+    return -1;
+  };
   
   this.addDesktop = function (desktop)
   {
-    if (this.ndesktops() > workspace.desktops) {return -1;};
-    this.desktop.push(desktop);
+    if (this.size() >= 1) {return -1;};
+    this.desktops.push(desktop);
     return 0;
   };
   
   this.removeDesktop = function (desktop_index)
   {
-    if (desktop_index > this.ndesktops()) {return -1;};
+    if (desktop_index >= this.ndesktops()) {return -1;};
     this.desktops.splice(desktop_index, 1);
     return 0;
   };
   
-  this.removeWindowId = function (window_id)
+  this.addTile = function (tile)
   {
-    var check = -1;
-    for (i = 0; i < this.ndesktops(); i++)
+    var i = this.availableId(tile.size);
+    if (i != -1 )
     {
-      check = this.desktops[i].removeTile(window_id);
-      if (check === 0) {break;};
+      this.desktops[i].addTile(tile);
+      return 0;
     };
-    return check;
+    var desktop = new Desktop();
+    if (this.addDesktop(desktop) === -1) {return -1;};
+    this.desktops[this.ndesktops() - 1].addTile(tile);
+    return 0;
   };
   
-  //returns the first desktop that has enough space
-  this.availableDesktop = function (size)
+  this.removeTile = function (desktop_index, tile_index)
   {
-    desktop = -1;
-    for (i = 0; i < this.ndesktops(); i++)
+    if (desktop_index >= this.ndesktops()) {return -1;};
+    return this.desktops[desktop_index].removeTile(tile_index);
+  };
+  
+  this.removeTileWindowId = function (window_id)
+  {
+    for (var i = 0; i < this.ndesktops(); i++)
     {
-      if (this.desktops[i].size() > size)
+      if (this.desktops[i].removeTileWindowId(window_id) === 0)
       {
-        desktop = i;
-        break;
-      }
-      return desktop;
+        if (this.desktops[i].size() === 0) {this.removeDesktop(i);};
+        return 0;
+      };
     };
-    
+    return -1;
   };
 };
 
@@ -161,56 +182,132 @@ function Layout ()
   this.layers = [];
   this.nlayers = function () {return this.layers.length;};
   
+  this.availableId = function (size)
+  {
+    for (var i = 0; i < this.nlayers(); i++)
+    {
+      if (this.layers[i].availableId(size) != -1) {return i;};
+    };
+    return -1;
+  };
+  
   this.addLayer = function (layer) 
   {
     this.layers.push(layer);
     return 0;
   };
   
+  this.removeLayer = function (layer_index)
+  {
+    if (layer_index >= this.nlayers()) {return -1;};
+    this.layers.splice(layer_index, 1);
+    return 0;
+  };
+  
   this.addDesktop = function (desktop)
   {
-    
-    
-    return 0;
+    for (var i = 0; this.nlayers(); i++)
+    {
+      if (this.layers[i].size() < 1) 
+      {
+        this.layers[i].addDesktop(desktop);
+        return 0;
+      };
+    };
+    return -1;
+  };
+  
+  this.removeDesktop = function (layer_index, desktop_index)
+  {
+    if (layer_index >= this.nlayers()) {return -1;};
+    return this.layers[layer_index].removeDesktop(desktop_index);
+  };
+  
+  this.addTile = function (tile)
+  {
+    var i = this.availableId(tile.size);
+    if (i != -1 )
+    {
+      this.layers[i].addTile(tile);
+      return 0;
+    };
+    var layer = new Layer();
+    this.addLayer(desktop);
+    return this.layers[this.nlayers() - 1].addTile(tile);
+  };
+  
+  this.removeTile = function (layer_index, desktop_index, tile_index)
+  {
+    if (layer_index >= this.nlayers()) {return -1;};
+    return this.layers[layer_index].removeTile(desktop_index, tile_index);
+  };
+  
+  this.removeTileWindowId = function (window_id)
+  {
+    for (var i = 0; i < this.nlayers(); i++)
+    {
+      if (this.layers[i].removeTileWindowId(window_id) === 0) {return 0;};
+    };
+    return -1;
   };
 };
 
-// start of script
+// -------------
+// Class Testing
+// -------------
 
-// execute when new client is opened
-workspace.clientAdded.connect
-(
-  function(client)
-  {
-//     var geometry = client.geometry
-//     geometry.x = client.x + gap + margins["left"];
-//     geometry.y = client.y + gap + margins["top"];
-//     geometry.width = client.width - gap * 2 - margins["left"] - margins["right"];
-//     geometry.height = client.height * 0.5 - gap - margins["top"];
-//     
-//   //   client.geometry = geometry
-//     
-//     print(client.windowId)
-//     print(client.caption)
-//     print('new client opened')
-//     print(client.geometry.x)
-//     print(client.geometry.y)
-//     print(client.geometry.width)
-//     print(client.geometry.height)
-    
-    var tile = new Tile(client.windowId, 0.5);
-    
-    print('-----')
-    var desktop = new Desktop();
-    desktop.addTile(tile);
-    desktop.addTile(tile);
-    print(desktop.size());
-    desktop.removeTile(1)
-    print(desktop.size());
-    desktop.removeWindowId(client.windowId);
-    print(desktop.size());
-    
-    
-  }
-);
+var layout = new Layout();
+
+var layer1 = new Layer();
+var layer2 = new Layer();
+
+var desktop1 = new Desktop();
+var desktop2 = new Desktop();
+
+var tile1 = new Tile(0, 0.25);
+var tile2 = new Tile(1, 0.5);
+
+print('Adding Tiles to Desktops');
+print(desktop1.addTile(tile1));
+print(desktop1.addTile(tile2));
+print(desktop2.addTile(tile1));
+print(desktop2.addTile(tile2));
+
+print('Adding Desktops to Layers');
+print(layer1.addDesktop(desktop1));
+print(layer1.addDesktop(desktop2));
+print(layer2.addDesktop(desktop1));
+print(layer2.addDesktop(desktop2));
+
+print('Adding Layers to Layout')
+print(layout.addLayer(layer1));
+print(layout.addLayer(layer2));
+
+// -----
+// Start
+// -----
+
+// // execute when new client is opened
+// workspace.clientAdded.connect
+// (
+//   function(client)
+//   {
+// //     var geometry = client.geometry
+// //     geometry.x = client.x + gap + margins["left"];
+// //     geometry.y = client.y + gap + margins["top"];
+// //     geometry.width = client.width - gap * 2 - margins["left"] - margins["right"];
+// //     geometry.height = client.height * 0.5 - gap - margins["top"];
+// //     
+// //   //   client.geometry = geometry
+// //     
+// //     print(client.windowId)
+// //     print(client.caption)
+// //     print('new client opened')
+// //     print(client.geometry.x)
+// //     print(client.geometry.y)
+// //     print(client.geometry.width)
+// //     print(client.geometry.height)
+// 
+//   };
+// );
 
